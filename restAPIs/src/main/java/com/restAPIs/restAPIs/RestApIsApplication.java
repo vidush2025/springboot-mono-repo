24353fbf405bfbf301bba1422c8e2c74a9b724ca
vidush2025 <vidushworks@gmail.com>
2026-02-05 00:31:30 +0530

    Starting with intro to rest APIs

---
+++ restAPIs/src/main/java/com/restAPIs/restAPIs/RestApIsApplication.java
@@ -0,0 +1,13 @@
+package com.restAPIs.restAPIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApIsApplication.class, args);
+	}
+
+}
