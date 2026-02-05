87fa5d3abfa5fe8cd889f564a9f10f358a58de19
vidush2025 <vidushworks@gmail.com>
2026-02-06 01:05:35 +0530

    postgresql setup and new Student entity and repository setup

--- restAPIs/src/main/java/com/restAPIs/restAPIs/controller/StudentController.java
+++ restAPIs/src/main/java/com/restAPIs/restAPIs/controller/StudentController.java
@@ -1,15 +1,23 @@
 package com.restAPIs.restAPIs.controller;
 
 import com.restAPIs.restAPIs.dto.StudentDto;
+import com.restAPIs.restAPIs.entity.Student;
+import com.restAPIs.restAPIs.repository.StudentRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 
 public class StudentController {
+    private final StudentRepository studentRepository;
+    public StudentController(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
 
     @GetMapping("/student")
-    public StudentDto getStudent(){
-        return new StudentDto(47L, "Vidush", "vidush@gmail.com");
+    public List<Student> getStudent(){
+        return studentRepository.findAll();
     }
 }
---
+++ restAPIs/src/main/java/com/restAPIs/restAPIs/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.restAPIs.restAPIs.repository;
+
+import com.restAPIs.restAPIs.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
